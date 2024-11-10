c26248e37d6ad32dac6f003833afe65105043b1c
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-11-10 21:08:26 +0600

    create Book service created

---
+++ src/app/modules/Book/book.services.ts
@@ -0,0 +1,13 @@
+import e from "express";
+import prisma from "../../shared/prisma";
+
+const createBookIntoDB = async (data: any) => {
+  const result = await prisma.book.create({
+    data,
+  });
+  return result;
+};
+
+export const BookServices = {
+  createBookIntoDB,
+};
