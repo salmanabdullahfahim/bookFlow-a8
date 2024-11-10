ef4d4a75b12b4f2399c87ea48d32adfdf5efff82
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-11-10 21:23:31 +0600

    create book controller created

---
+++ src/app/modules/Book/book.controllers.ts
@@ -0,0 +1,19 @@
+import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { BookServices } from "./book.services";
+import { StatusCodes } from "http-status-codes";
+
+const createBook = catchAsync(async (req, res, next) => {
+  const result = await BookServices.createBookIntoDB(req.body);
+
+  sendResponse(res, {
+    statusCode: StatusCodes.CREATED,
+    success: true,
+    message: "Book created successfully",
+    data: result,
+  });
+});
+
+export const BookControllers = {
+  createBook,
+};
